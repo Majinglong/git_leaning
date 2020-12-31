a20c685b8a52280f86569e42190f64b4762080f0
xichuan <1874425078@qq.com>
2020-12-31 15:18:37 +0800

    'add'

---
+++ src/main/java/com/machine/xuegit/XuegitApplication.java
@@ -0,0 +1,13 @@
+package com.machine.xuegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuegitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuegitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/machine/xuegit/XuegitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machine.xuegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuegitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
